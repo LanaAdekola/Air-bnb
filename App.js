d774142d8b15a6eca078b502e981305e3b1248f4
LanaAdekola <adekolaadelana@gmail.com>
2023-04-04 00:48:51 +0100

    .

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Card from "./component/Cards"
+import Nav from "./component/Nav"
+import Hero from "./component/Hero"
+import data from "./data"
+
