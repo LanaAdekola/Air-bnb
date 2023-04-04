b35344cb47cd9dfce7f8d6a956c6af7d28b848bc
LanaAdekola <adekolaadelana@gmail.com>
2023-04-04 02:00:32 +0100

    .

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
+
+
 import React from "react"
-import ReactDOM from "react-dom"
-import Card from "./component/Cards"
-import Nav from "./component/Nav"
-import Hero from "./component/Hero"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 import data from "./data"
 
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
